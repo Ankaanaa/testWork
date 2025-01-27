c51743eb0ad0051c413a0202800b05c9f494b5a9
Dmitriy <admin@gmail.com>
2025-01-27 02:51:37 +0100

    Сохранения выбраных элементов и последующие их поднятие их вверх при выборе

--- src/editUsers/EditUsers.tsx
+++ src/editUsers/EditUsers.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { users } from '../data/data'
 
 import './EditUsers.scss'
@@ -10,6 +10,15 @@ interface users {
 	country: string
 	status: string
 }
+interface data {
+	name: string
+	value: string
+}
+interface indexAndValue {
+	value: string
+	index: number
+	name: string
+}
 
 const EditUsers = () => {
 	const [select, setSelect] = useState('')
@@ -19,25 +28,100 @@ const EditUsers = () => {
 	const [country, setCountry] = useState('')
 	const [status, setStatus] = useState('')
 	const [lengthName, setLengthName] = useState(true)
+	const [dataStatuses, setDataStatuses] = useState<data[]>([])
+	const [dataCountries, setDataCountries] = useState<data[]>([])
+	const [dataDepartments, setDataDepartments] = useState<data[]>([])
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const fetchStatus = await fetch('/Statuses.json')
+				const fetchCountries = await fetch('/Countries.json')
+				const fetchDepartments = await fetch('/Departments.json')
+
+				const status = await fetchStatus.json()
+				const countries = await fetchCountries.json()
+				const departments = await fetchDepartments.json()
+
+				setDataStatuses(status)
+				setDataCountries(countries)
+				setDataDepartments(departments)
+			} catch (err) {
+				console.log('Произошла ошибка при загрузке данных', err)
+			}
+		}
+		fetchData()
+	}, [])
+
+	const UpSelect = (
+		arg: indexAndValue,
+		setData: React.Dispatch<React.SetStateAction<data[]>>
+	) => {
+		if (arg) {
+			if (arg.index !== 0) {
+				setData(prevData => {
+					const newData = [...prevData]
+					newData.splice(arg.index, 1)
+					newData.unshift({ name: arg.name, value: arg.value })
+					return newData
+				})
+			}
+		}
+	}
+	const SaveParams = (
+		event: React.ChangeEvent<HTMLSelectElement>,
+		data: data[]
+	) => {
+		const selectedData = data
+			.map((el: data, index: number) => ({
+				value: el.value,
+				index: index,
+				name: el.name,
+			}))
+			.find((el: any) => el.name === event.target.value)
+
+		return selectedData
+	}
+
+	const UserInfoUp = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const IndexUs = parseInt(event.target.value, 10)
+		const check = dataDepartments.findIndex(
+			(index: data) => users[IndexUs].department === index.name
+		)
+		const { name, value } = dataDepartments[check]
+		if (check !== -1 && check !== 0) {
+			setDataDepartments(prev => {
+				const newData = [...prev]
+				newData.splice(check, 1)
+				newData.unshift({ name: name, value: value })
+				return newData
+			})
+		}
+		return check
+	}
 
 	const ChangeUser = (
 		event: React.ChangeEvent<HTMLSelectElement>,
-		inputName: string
+		inputName: string,
+		data?: indexAndValue
 	) => {
 		if (inputName === 'user') {
 			const index = parseInt(event.target.value, 10)
 			setIndex(index)
 			setSelect(event.target.value)
-			console.log(select)
+			UserInfoUp(event)
 			setName(users[index].name)
 			setDepartment(users[index].department)
 			setCountry(users[index].country)
 			setStatus(users[index].status)
 		} else if (inputName === 'department') {
+			UpSelect(data as indexAndValue, setDataDepartments)
 			setDepartment(event.target.value)
 		} else if (inputName === 'country') {
+			UpSelect(data as indexAndValue, setDataCountries)
 			setCountry(event.target.value)
 		} else if (inputName === 'status') {
+			UpSelect(data as indexAndValue, setDataStatuses)
 			setStatus(event.target.value)
 		}
 	}
@@ -76,36 +160,58 @@ const EditUsers = () => {
 						<div>Department</div>
 						<select
 							value={department}
-							onChange={event => ChangeUser(event, 'department')}
+							onChange={event => {
+								ChangeUser(
+									event,
+									'department',
+									SaveParams(event, dataDepartments)
+								)
+							}}
 							className='editUsers__select'
 						>
-							<option value='Programming'>Digital Marketing</option>
-							<option value='Backend'>Backend</option>
-							<option value='Frontend'>Frontend</option>
+							{dataDepartments.map((el: data, index: number) => {
+								return (
+									<option key={index} value={el.name}>
+										{el.name}
+									</option>
+								)
+							})}
 						</select>
 					</div>
 					<div className='editUsers__column'>
 						<div>Country</div>
 						<select
 							value={country}
-							onChange={event => ChangeUser(event, 'country')}
+							onChange={event =>
+								ChangeUser(event, 'country', SaveParams(event, dataCountries))
+							}
 							className='editUsers__select'
 						>
-							<option value='Ukraine'>United State</option>
-							<option value='USA'>USA</option>
-							<option value='Poland'>Poland</option>
+							{dataCountries.map((el: data, index: number) => {
+								return (
+									<option key={index} value={el.name}>
+										{el.name}
+									</option>
+								)
+							})}
 						</select>
 					</div>
 					<div className='editUsers__column'>
 						<div>Status</div>
 						<select
 							value={status}
-							onChange={event => ChangeUser(event, 'status')}
+							onChange={event =>
+								ChangeUser(event, 'status', SaveParams(event, dataStatuses))
+							}
 							className='editUsers__select'
 						>
-							<option value='Active'>Active</option>
-							<option value='No active'>No active</option>
-							<option value='Disabled'>Disabled</option>
+							{dataStatuses.map((el: data, index: number) => {
+								return (
+									<option key={index} value={el.name}>
+										{el.name}
+									</option>
+								)
+							})}
 						</select>
 					</div>
 				</div>
